83daa6732802b5aa36ad0971ff9ea7715f9d0718
Alexey Ryabov <lesha_12_01@mail.ru>
2019-03-26 19:00:08 +0300

    chore: add Element to Window's interface

--- src/app/models/global.d.ts
+++ src/app/models/global.d.ts
@@ -6,3 +6,11 @@ declare module '*.scss' {
   const classNames: IClassNames;
   export = classNames;
 }
+
+declare module '*.{js,jsx,ts,tsx}' {
+  declare global {
+    interface Window {
+      Element: any;
+    }
+  }
+}
