2422bc5f312f435db094b0073a6d549c0b2453cd
Alexey Ryabov <lesha_12_01@mail.ru>
2019-05-05 19:10:19 +0300

    chore: add index for shared components

---
+++ src/app/shared/components/index.ts
@@ -0,0 +1,11 @@
+export { default as ButtonIcon } from './ButtonIcon';
+export { default as Drawer } from './Drawer';
+export { default as FormContainer } from './FormContainer';
+export { default as Header } from './Header';
+export { default as Logo } from './Logo';
+
+export * from './ButtonIcon';
+export * from './Drawer';
+export * from './FormContainer';
+export * from './Header';
+export * from './Logo';
