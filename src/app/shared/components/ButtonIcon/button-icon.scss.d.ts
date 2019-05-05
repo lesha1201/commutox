1df3ad07317a16ddb5596fb8c3163c474328b902
Alexey Ryabov <lesha_12_01@mail.ru>
2019-05-05 19:03:13 +0300

    feat: add <ButtonIcon /> component

---
+++ src/app/shared/components/ButtonIcon/button-icon.scss.d.ts
@@ -0,0 +1,6 @@
+// This file is generated automatically
+export interface IButtonIconScss {
+  base: string;
+}
+declare const styles: IButtonIconScss;
+export default styles;
---
+++ src/app/shared/components/ButtonIcon/index.tsx
@@ -0,0 +1,27 @@
+import cx from 'classnames';
+import * as React from 'react';
+
+import { OverwritableType } from 'app/models/common';
+import css from './button-icon.scss';
+
+export interface IButtonIconProps<T> {
+  as: T;
+  children?: React.ReactNode;
+}
+
+function ButtonIcon<T extends React.ReactType = 'button'>({
+  className,
+  as,
+  ...domAttrs
+}: OverwritableType<IButtonIconProps<T>, T>) {
+  const cn = cx(css.base, className);
+  const ElementType: React.ReactType = as;
+
+  return <ElementType className={cn} {...domAttrs} />;
+}
+
+ButtonIcon.defaultProps = {
+  as: 'button',
+};
+
+export default ButtonIcon;
