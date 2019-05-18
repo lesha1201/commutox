4b013467ae7532236cfa07a3fc15c2b3c2f53d9b
Alexey Ryabov <lesha_12_01@mail.ru>
2019-05-18 13:27:21 +0300

    feat(feed): add <MessageCard />
    Remove unnessary files in feed folder

---
+++ src/app/pages/feed/components/MessageCard/index.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Counter } from 'app/shared/components';
+import { Avatar } from 'app/shared/ui-kit';
+import css from './message-card.scss';
+
+interface IProps {
+  src: string;
+  newMessageCount: number;
+  showStatus: boolean;
+  isOnline: boolean;
+  messageInfo: {
+    name: string;
+    time: number | string;
+    content: string;
+  };
+}
+
+function MessageCard({
+  newMessageCount,
+  messageInfo,
+  showStatus,
+  isOnline,
+  src,
+}: IProps) {
+  return (
+    <Link to="#" className={newMessageCount > 0 ? css.newMessage : css.normal}>
+      <Avatar
+        className={css.avatar}
+        showStatus={showStatus}
+        isOnline={isOnline}
+        src={src}
+      />
+
+      <div className={css.info}>
+        <div className={css.row}>
+          <div className={css.name}>{messageInfo.name}</div>
+          <div className={css.time}>{messageInfo.time}</div>
+        </div>
+
+        <div className={css.row}>
+          <div className={css.message}>{messageInfo.content}</div>
+          <Counter className={css.counter} isInverted={true}>
+            {newMessageCount}
+          </Counter>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+MessageCard.defaultProps = {
+  newMessageCount: 0,
+  isOnline: false,
+  showStatus: false,
+};
+
+export default MessageCard;
---
+++ src/app/pages/feed/components/MessageCard/message-card.scss.d.ts
@@ -0,0 +1,14 @@
+// This file is generated automatically
+export interface IMessageCardScss {
+  'normal': string;
+  'avatar': string;
+  'info': string;
+  'row': string;
+  'name': string;
+  'message': string;
+  'time': string;
+  'counter': string;
+  'newMessage': string;
+}
+declare const styles: IMessageCardScss;
+export default styles;
