8785fd68ef2fe4e76a0e94a44128e7bec81c94b5
Alexey Ryabov <lesha_12_01@mail.ru>
2019-05-18 13:58:54 +0300

    feat(feed): finish base UI of the page

--- src/app/pages/feed/feed.scss.d.ts
+++ src/app/pages/feed/feed.scss.d.ts
@@ -1,1 +1,8 @@
 // This file is generated automatically
+export interface IFeedScss {
+  'header': string;
+  'headerWrapper': string;
+  'logo': string;
+}
+declare const styles: IFeedScss;
+export default styles;
--- src/app/pages/feed/index.tsx
+++ src/app/pages/feed/index.tsx
@@ -1,16 +1,61 @@
 import * as React from 'react';
 
-// import * as style from './feed.scss';
+import { ButtonIcon, Header, Logo } from 'app/shared/components';
+import Icon from 'app/shared/icons';
 
-// Temporary
-import Design from './design';
+import Drawer, { useDrawer } from './components/Drawer';
+import MessageCard from './components/MessageCard';
+import css from './feed.scss';
 
-const FeedPage: React.FC<{}> = () => {
-  return (
-    // <div className={style.feed}>
-    <Design />
-    // </div>
+const { useMemo } = React;
+
+function FeedPage() {
+  const { isShown, closeDrawer, toggleDrawer } = useDrawer(false);
+  // Memoize main content in order to avoid rerender when [isShown] changes
+  const mainContent = useMemo(
+    () => (
+      <>
+        <Header className={css.header}>
+          <ButtonIcon onClick={toggleDrawer}>
+            <Icon name="menu" />
+          </ButtonIcon>
+
+          <div className={css.logo}>
+            <Logo height="100%" />
+          </div>
+
+          <ButtonIcon>
+            <Icon name="search" />
+          </ButtonIcon>
+        </Header>
+
+        <Header.Wrapper className={css.headerWrapper}>
+          {new Array(20).fill(0).map((el, inx) => (
+            <MessageCard
+              key={inx}
+              newMessageCount={inx === 0 ? 3 : 0}
+              isOnline={Boolean(Math.round(Math.random()))}
+              showStatus={true}
+              src="https://format-com-cld-res.cloudinary.com/image/private/s--4LNnIVDC--/c_limit,g_center,h_65535,w_550/a_auto,fl_keep_iptc.progressive,q_95/v1/575114c96aba3e63085150d7ab14a0e0/MILLIE-CONSTANCE-VICTORIA-EDIT-1web.jpg"
+              messageInfo={{
+                name: 'Name Surname',
+                content: 'Hello! How are you?',
+                time: '15 min. ago',
+              }}
+            />
+          ))}
+        </Header.Wrapper>
+      </>
+    ),
+    [],
   );
-};
+
+  return (
+    <>
+      <Drawer isShown={isShown} close={closeDrawer} />
+      {mainContent}
+    </>
+  );
+}
 
 export default FeedPage;
